fcbec24f6de898caae8f9539e004f07899280419
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-19 03:34:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,6 @@ def literal_aoh
   # held in the constants
   array = [{:name=>"Don Gately", :occupation=>"Live-in Staff"},{:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},{:name=>"Pat Monteseian", :occupation=>"Staff"},{:name=>"Kate Gompert", :occupation=>"None"},{:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
     ]
-  
 end
 
 def aoh_lookup(aoh, row, key)
