8def2e9d84e87b6b1d258ee2e14e5404aafa9a2c
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-19 02:55:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,6 +12,7 @@ def assembled_aoh
   
   array = [{:name=>"Don Gately", :occupation=>"Live-in Staff"},{:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},{:name=>"Pat Monteseian", :occupation=>"Staff"},{:name=>"Kate Gompert", :occupation=>"None"},{:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
     ]
+    array
 end
 
 def literal_aoh
